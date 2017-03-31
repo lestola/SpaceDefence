352c008c16c3a9e582e6e5fbce886d157beb124c
xialeksi <xial.aleksi@gmail.com>
2017-03-31 13:28:53 +0300

    Viimeisimmät koodailut 13.28

--- SpaceDefense/Assets/Scripts/EnemyBeamScript.cs
+++ SpaceDefense/Assets/Scripts/EnemyBeamScript.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBeamScript : MonoBehaviour {
-
+public class EnemyBeamScript : MonoBehaviour
+{
+    float timer = 0;
     SpriteRenderer sr;
+    public bool IsShooting = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         sr = GetComponent<SpriteRenderer>();
-	}
+    }
 
+    void Update()
+    {
+        if (IsShooting == true)
+        {
+            timer = timer + 1* Time.deltaTime;
+        }
+        if (timer > 1)
+        {
+            IsShooting = false;
+        }
+        if (IsShooting == true)
+            sr.enabled = true;
+        else
+            sr.enabled = false;
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            //make this spriteRenderer visible, do take damage function from within player
-            sr.enabled=true;
-
+            IsShooting = true;
         }
     }
 }
